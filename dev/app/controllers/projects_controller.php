1ed0ebb48172ec16eef0c2cd613113d144e89d50
gaude.emmanuel <EMegamanu@users.noreply.github.com>
2009-11-07 19:01:16 +0000

    Réorganisation add Project pour plus de clareté.

--- dev/app/controllers/projects_controller.php
+++ dev/app/controllers/projects_controller.php
@@ -29,22 +29,42 @@ class ProjectsController extends AppController
 		$this->Project->Team->find('first',name);
     }	
 	
+	private function getRoleId($roleName) {
+		$params = array (
+			'conditions' => array(
+				'Role.name' => $roleName
+			),
+			'fields' => array('Role.id')
+		);
+		$resultRole = $this->Role->find('first', $params);
+		
+		/* Test si un id de rôle a bien été retourné. */
+		if(empty($resultRole)) {
+			return -1;
+		} else {
+			return $resultRole['Role']['id'];
+		}
+	}
+
+	/**
+	 * Ajout d'un nouveau projet à la base.
+	 *
+	 * @pre		Demande de création d'un nouveau projet par le biais du 
+	 * 			formulaire de la vue.
+	 * @post	Crée le projet et une équipe mère associée, tous deux
+	 *			administrés par l'utilisateur courant (chef de projet et chef
+	 *			équipe)
+	 */
     public function add()
     {
 		if(!empty($this->data))
 		{
-			/* On cherche l'ID du rôle correspondant à un chef de projet. */
+			/* 1 - Recherche des id des rôles propres à un chef de projet. -- */
 			$this->loadModel('Role');
-			$paramsProjectAdmin = array (
-				'conditions' => array(
-					'Role.name' => 'project_admin'
-				),
-				'fields' => array('Role.id')
-			);
-			$resultRole = $this->Role->find('first', $paramsProjectAdmin);
-			
-			/* Test si un id de rôle a bien été retourné. */
-			if(empty($resultRole)) {
+		
+			/* On cherche l'ID du rôle correspondant à un chef de projet. */	
+			$idRoleProjectAdmin = $this->getRoleId('project_admin');
+			if($idRoleProjectAdmin == -1) {
 				$this->flash(
 					'Le projet n\'a pu être créé car aucun rôle ' +
 					'`project_admin` n\'existe dans la base.',
@@ -52,93 +72,96 @@ class ProjectsController extends AppController
 				);
 				return;
 			}
-			$idRoleProjectAdmin = $resultRole['Role']['id'];
 			
-			
-			/* Ajout du projet à la BDD. */
-			$retAddProject = $this->Project->save($this->data);
-			$retAddProjectAdmin = false;
-			$retAddTeam = false;
-			$retUpProject = false;
-			
-			if($retAddProject) {
-				$this->data['Project']['id'] = $this->Project->id;
-				
-				/* 
-				 * On ajoute le chef de projet :
-				 *  - ID utilisateur courant
-				 *  - ID du rôle 'project_admin'
-				 */
-				$retAddProjectAdmin = $this->Project->ProjectsUser->save(Array(
-					'project_id' => $this->Project->id,
-					'user_id' => $this->Auth->user('id'),
-					'role_id' => $idRoleProjectAdmin
-				));
-				
-				/* Ajout de l'équipe mère du projet. */
-				$retAddTeam = $this->Project->Team->save(Array(
-					'name' => 'Equipe ' . $this->data['Project']['name'],
-					'description' => 'Equipe du projet [' .
-									 $this->data['Project']['id'] . '] "' .
-									 $this->data['Project']['name'] . '"',
-					'project_id' => $this->data['Project']['id'],
-					'user_id' => $this->Auth->user('id')
-				));
-				
-				if($retAddTeam) {
-					/* On cherche l'ID du rôle correspondant à un chef d'équipe. */
-					$paramsTeamAdmin = array (
-						'conditions' => array(
-							'Role.name' => 'team_admin'
-						),
-						'fields' => array('Role.id')
-					);
-					$resultRole = $this->Role->find('first', $paramsTeamAdmin);
-					
-					/* Test si un id de rôle a bien été retourné. */
-					if(empty($resultRole)) {
-						$this->flash(
-							'Le projet n\'a pu être créé car aucun rôle ' +
-							'`team_admin` n\'existe dans la base.',
-							'/projects'
-						);
-						return;
-					}
-					$idRoleTeamAdmin = $resultRole['Role']['id'];
-					
-					$retAddTeamAdmin = $this->Project->Team->TeamsUser->save(Array(
-						'team_id' => $this->Project->Team->id,
-						'user_id' => $this->Auth->user('id'),
-						'role_id' => $idRoleTeamAdmin
-					));
-				
-					/* MàJ du projet avec l'id de l'équipe. */	
-					$this->data['Project']['team_id'] = $this->Project->Team->id;
-				
-					$retUpProject = $this->Project->save($this->data);
-				}
-			}
-			
-			/* Message de confirmation en cas de succès. */
-			if($retAddProject && $retAddTeam && $retUpProject) {
+			/* On cherche l'ID du rôle correspondant à un chef d'équipe. */	
+			$idRoleTeamAdmin = $this->getRoleId('team_admin');
+			if($idRoleTeamAdmin == -1) {
 				$this->flash(
-					'Le projet a bien été ajouté et sauvegardé.',
+					'Le projet n\'a pu être créé car aucun rôle ' +
+					'`team_admin` n\'existe dans la base.',
 					'/projects'
 				);
-			} else {
-				/* 
-				   Suppression des entrées créées en cas d'échec lors d'une 
-				   étape.
-				 */
-				if($retAddTeam) {
-					$this->Project->Team->del($this->Project->Team->id);
-				}
-				$this->Project->del($this->Project->id);
+				return;
+			}
+			
+			/* 2 - Création du projet et de l'équipe mère. ------------------ */
+			/* Création du projet dans la BDD. */
+			if(!$this->Project->save($this->data)) {
 				$this->flash(
 					'Le projet n\'a pu être créé.',
 					'/projects'
 				);
+				return;
 			}
+			
+			/* Création de l'équipe mère du projet. */
+			if(!$this->Project->Team->save(Array(
+				'name' => 'Equipe ' . $this->data['Project']['name'],
+				'description' => 'Equipe du projet [' .
+								 $this->data['Project']['id'] . '] "' .
+								 $this->data['Project']['name'] . '"',
+				'project_id' => $this->Project->id,
+				'user_id' => $this->Auth->user('id')
+			))) {
+				$this->Project->del($this->Project->id);
+				$this->flash(
+					'L\'équipe associée au projet n\'a pu être créée.',
+					'/projects'
+				);
+				return;
+			}
+			
+			/* 3 - Association du projet et de l'équipe. -------------------- */
+			$this->data['Project']['id'] = $this->Project->id;
+			$this->data['Project']['team_id'] = $this->Project->Team->id;
+			if(!$this->Project->save($this->data)) {
+				$this->Project->Team->del($this->Project->Team->id);
+				$this->Project->del($this->Project->id);
+				$this->flash(
+					'L\'équipe mère n\'a pu être associée au proket.',
+					'/projects'
+				);
+				return;
+			}
+			
+			/* 4 - Association utilisateur comme chef de projet et d'équipe.  */
+			/* Association du chef de projet. */
+			if(!$this->Project->ProjectsUser->save(Array(
+				'project_id' => $this->Project->id,
+				'user_id' => $this->Auth->user('id'),
+				'role_id' => $idRoleProjectAdmin
+			))) {
+				$this->Project->Team->del($this->Project->Team->id);
+				$this->Project->del($this->Project->id);
+				$this->flash(
+					'L\'utilisateur courant n\'a pu être associé comme chef ' +
+					' de projet.',
+					'/projects'
+				);
+				return;
+			}
+			
+			/* Association du chef d'équipe. */
+			if(!$this->Project->Team->TeamsUser->save(Array(
+				'team_id' => $this->Project->Team->id,
+				'user_id' => $this->Auth->user('id'),
+				'role_id' => $idRoleTeamAdmin
+			))) {
+				$this->Project->Team->del($this->Project->Team->id);
+				$this->Project->del($this->Project->id, true);
+				$this->flash(
+					'L\'utilisateur courant n\'a pu être associé comme chef ' +
+					' d\équipe.',
+					'/projects'
+				);
+				return;
+			}
+			
+			/* 5 - Message de confirmation en cas de succès. ---------------- */
+			$this->flash(
+				'Le projet a bien été ajouté et sauvegardé.',
+				'/projects'
+			);
 		}
     }
 
